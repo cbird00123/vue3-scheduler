b261a09ce3118a8a715e9dc0f80bc8f91cc4b91e
Hsm07 <hsmin0703@oneplat.co>
2024-01-16 20:08:32 +0900

    기본 세팅 package installed

--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
-import './assets/main.css'
+import './assets/main.scss'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+
+import { Quasar } from 'quasar'
+import quasarLang from 'quasar/lang/ko-KR'
+
+// Import icon libraries
+import '@quasar/extras/material-icons/material-icons.css'
+
+// Import Quasar css
+import 'quasar/src/css/index.sass'
 
 import App from './App.vue'
 import router from './router/index.js'
@@ -10,5 +19,9 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(Quasar, {
+  plugins: {},
+  lang: quasarLang,
+})
 
 app.mount('#app')
